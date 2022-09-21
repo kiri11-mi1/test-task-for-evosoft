ce23aa619527ba6f3863d9dac4707eaefc978396
Kirill Mil <kirill.mil.0310@mail.ru>
2022-09-21 21:27:35 +0700

    add notes fixtures

--- src/apps/journals/tests/fixtures.py
+++ src/apps/journals/tests/fixtures.py
@@ -1,6 +1,6 @@
 import pytest
 from mixer.backend.django import mixer
-from apps.journals.models import Diary
+from apps.journals.models import Diary, Note
 
 
 @pytest.fixture
@@ -13,3 +13,11 @@ def diaries():
     return mixer.cycle(5).blend(Diary)
 
 
+@pytest.fixture
+def note():
+    return mixer.blend(Note)
+
+
+@pytest.fixture
+def notes():
+    return mixer.cycle(5).blend(Note)
