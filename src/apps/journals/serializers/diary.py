83544b55699c7b93a01ba1aa9bb52b8b912af00b
Kirill Mil <kirill.mil.0310@mail.ru>
2022-09-21 16:49:35 +0700

    fix validator

--- src/apps/journals/serializers/diary.py
+++ src/apps/journals/serializers/diary.py
@@ -11,10 +11,20 @@ class DiaryCreateSerializer(serializers.ModelSerializer):
         expiration = data.get('expiration')
         kind = data.get('kind')
 
+        if not expiration and self.instance:
+            expiration = self.instance.expiration
+    
         if expiration and kind == DiaryTypes.PUBLIC:
             raise serializers.ValidationError(DiaryErrors.ONLY_PRIVATE)
 
         return data
+
+    def update(self, instance, validated_data):
+        instance.kind = validated_data.get('kind', instance.kind)
+        instance.expiration = validated_data.get('expiration', instance.expiration)
+        instance.title = validated_data.get('title', instance.title)
+        instance.save()
+        return instance
 
     def create(self, validated_data: dict):
         validated_data['user'] = self.context['request'].user
