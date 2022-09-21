db0ed99b82b377b3006f6ec3dce538b4a6cc6b29
Kirill Mil <kirill.mil.0310@mail.ru>
2022-09-21 21:50:38 +0700

    add filter backend

--- src/apps/journals/viewsets/diary.py
+++ src/apps/journals/viewsets/diary.py
@@ -1,8 +1,10 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets, pagination
+from django_filters.rest_framework import DjangoFilterBackend
 
 from ..models import Diary
 from ..serializers import DiarySerializer
+from ..filters import DiaryFilter
 
 
 class DiaryViewSet(viewsets.ModelViewSet):
@@ -10,6 +12,8 @@ class DiaryViewSet(viewsets.ModelViewSet):
     pagination_class.default_limit = 20
     permission_classes = (IsAuthenticated,)
     serializer_class = DiarySerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = DiaryFilter
 
     def get_queryset(self):
         queryset = Diary.objects.filter(user=self.request.user)
