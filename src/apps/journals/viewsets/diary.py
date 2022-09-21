22732f82f78c93e62e36fc454625cc3ce33a8ee8
Kirill Mil <kirill.mil.0310@mail.ru>
2022-09-21 15:50:59 +0700

    add pagination

--- src/apps/journals/viewsets/diary.py
+++ src/apps/journals/viewsets/diary.py
@@ -1,11 +1,13 @@
-from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, pagination
 
 from ..models import Diary
 from ..serializers import DiaryReadSerializer, DiaryCreateSerializer
 
 
 class DiaryViewSet(viewsets.ModelViewSet):
+    pagination_class = pagination.LimitOffsetPagination
+    pagination_class.default_limit = 20
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
